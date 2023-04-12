09ad0a4e25a7f1c710b0b5a4491e295be7b7ceb4
Hobyy19 <arivonyhoby@gmail.com>
2023-04-12 22:36:43 +0300

    Traitement message d'erreur

--- src/main/java/mg/itu/tpbanquehoby/jsf/TransfertArgent.java
+++ src/main/java/mg/itu/tpbanquehoby/jsf/TransfertArgent.java
@@ -6,6 +6,9 @@ package mg.itu.tpbanquehoby.jsf;
 
 import jakarta.ejb.EJB;
 import jakarta.enterprise.context.RequestScoped;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
+import jakarta.faces.context.Flash;
 import jakarta.inject.Named;
 import mg.itu.tpbanquehoby.ejb.GestionnaireCompte;
 import mg.itu.tpbanquehoby.entities.CompteBancaire;
@@ -55,8 +58,37 @@ public class TransfertArgent {
     
     public String validerTransfert(){
         CompteBancaire source = gestionnaireCompte.findCompte(idSource);
+        if(source == null){
+            FacesMessage message = new FacesMessage("Pas de compte avec id "+idSource);
+            // throw new ValidatorException(message);
+            addFlashMessage(message);
+            return null;
+        } else {
+            if(source.getSolde() < montant){
+                FacesMessage message = new FacesMessage("Solde insuffisant pour cette opération ");
+                // throw new ValidatorException(message);
+                addFlashMessage(message);
+                return null;
+            }
+        }
+        
         CompteBancaire destinataire = gestionnaireCompte.findCompte(idDestinataire);
+        if( destinataire == null){
+            FacesMessage message = new FacesMessage("Pas de compte avec id "+idDestinataire);
+            // throw new ValidatorException(message);
+            addFlashMessage(message);
+            return null;
+        }
         gestionnaireCompte.transferer(source, destinataire, montant);
-        return "listeComptes";
+        FacesMessage message = new FacesMessage("Transfert d'un montant de "+montant+" de "+source.getNom()+" vers "+destinataire.getNom()+" effectué avec succès.");
+        addFlashMessage(message);
+        return "listeComptes?faces-redirect=true";
+    }
+    
+     public static void addFlashMessage(FacesMessage message) {
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        Flash flash = facesContext.getExternalContext().getFlash();
+        flash.setKeepMessages(true);
+        facesContext.addMessage(null, message);
     }
 }
